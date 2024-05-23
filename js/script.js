bc2ec9c7e85778bcf38a69cfd20e484945e27a9a
Diego <1321382@utp.edu.pe>
2024-05-23 12:37:34 -0500

    agregando footer y validando el commit aca

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function cargarFragmento(idContenedor, rutaHTML){
+    fetch(rutaHTML)
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById(idContenedor).innerHTML = html;
+    });
+}
+//CARGAR FOOTER
+cargarFragmento('footerPlaceholder','/components/footer.html');
